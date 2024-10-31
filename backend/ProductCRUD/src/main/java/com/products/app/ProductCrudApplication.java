81bbf9ccb312c0d3341a53c6ea55b2dd1ad90479
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-10-31 09:49:16 -0300

    make a helloWorldController and crating a index.html file

--- backend/ProductCRUD/src/main/java/com/products/app/ProductCrudApplication.java
+++ backend/ProductCRUD/src/main/java/com/products/app/ProductCrudApplication.java
@@ -2,8 +2,10 @@ package com.products.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.products", "com.products.app"})
 public class ProductCrudApplication {
 
 	public static void main(String[] args) {
