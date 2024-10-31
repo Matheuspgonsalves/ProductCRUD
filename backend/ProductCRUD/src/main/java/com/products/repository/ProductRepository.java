67f5cc7da15f5053a59228d299387f19a5b98fdc
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-10-31 13:11:52 -0300

    Repository de product criado

---
+++ backend/ProductCRUD/src/main/java/com/products/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.products.repository;
+
+import com.products.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
