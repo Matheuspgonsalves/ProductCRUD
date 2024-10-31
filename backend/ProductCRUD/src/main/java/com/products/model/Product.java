0106f57d56ec7383a48bb78f735725c5990a57a2
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-10-31 13:04:29 -0300

    Model product criado. Getters e Setters definidos

---
+++ backend/ProductCRUD/src/main/java/com/products/model/Product.java
@@ -0,0 +1,20 @@
+package com.products.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private String description;
+    private int total;
+    private byte[] image;
+}
